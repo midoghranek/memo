719103a065299fb9c7187e1d1a1fa0f41bc719d5
midoghranek <midoghranek@hotmail.com>
2019-06-16 10:30:40 +0200

    my first commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  memes: [
+    {
+      userHandle: "user",
+      body: "this is the meme body",
+      createdAt: "2019-06-14T09:45:10.966Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
